9a0296f485ef3f9359b22d3ee318a1cca4875bcd
Tangui Morlier <tangui@tangui.eu.org>
2026-01-27 17:44:13 +0100

    Evite les méprise avec les chemin de répertoire

--- replace.sh
+++ replace.sh
@@ -19,31 +19,29 @@ python3 generate_grille.py "$MESSAGE" "${PREFIX}_"
 sed -i -f motifs.sed "$GRILLE"
 
 
-[[ "$PATH_PAGES" != */ ]] && PATH_PAGES="${PATH_PAGES}/"
-
-if [ ! -f "${PATH_PAGES}page1.svg" ]; then
+if [ ! -f "${PATH_PAGES}/page1.svg" ]; then
   echo "page1.svg not found!"
   exit 1
 fi
 
-if [ ! -f "${PATH_PAGES}page2.svg" ]; then
+if [ ! -f "${PATH_PAGES}/page2.svg" ]; then
   echo "page2.svg not found!"
   exit 1
 fi
 
-if [ ! -f "${PATH_PAGES}page3.svg" ]; then
+if [ ! -f "${PATH_PAGES}/page3.svg" ]; then
   echo "page3.svg not found!"
   exit 1
 fi
 
-if [ ! -f "${PATH_PAGES}page4.svg" ]; then
+if [ ! -f "${PATH_PAGES}/page4.svg" ]; then
   echo "page4.svg not found!"
   exit 1
 fi
 
 mkdir -p "/tmp/$DESTINATAIRE"
 
-sed "s/%NOM%/$DESTINATAIRE/" ${PATH_PAGES}page1.svg > /tmp/"$DESTINATAIRE"/page1.svg
+sed "s/%NOM%/$DESTINATAIRE/" ${PATH_PAGES}/page1.svg > /tmp/"$DESTINATAIRE"/page1.svg
 
 MOTS_VERTI=$(jq -r '.[] | select(.orientation=="V") | "\(.mot_id). \(.definition)"' "$JSON_MOTS" | tr "\n" '|' | sed 's/|/\\n/g')
 
@@ -52,14 +50,14 @@ MOTS_HORIZ=$(jq -r '.[] | select(.orientation=="H") | "\(.mot_id). \(.definition
 SOLUTIONS=$(jq -r '.[] | "\(.mot_id). \(.mot)"' "$JSON_MOTS" | tr "\n" ' ')
 
 mkdir -p "tmp/$DESTINATAIRE"
-sed -e "s/%MOTS_VERTI%/$MOTS_VERTI/" -e "s/%MOTS_HORIZ%/$MOTS_HORIZ/" ${PATH_PAGES}page2.svg > tmp/"$DESTINATAIRE"/page2.svg
-sed "s/%SOLUTIONS%/$SOLUTIONS/" ${PATH_PAGES}page4.svg > tmp/"$DESTINATAIRE"/page4.svg
+sed -e "s/%MOTS_VERTI%/$MOTS_VERTI/" -e "s/%MOTS_HORIZ%/$MOTS_HORIZ/" ${PATH_PAGES}/page2.svg > tmp/"$DESTINATAIRE"/page2.svg
+sed "s/%SOLUTIONS%/$SOLUTIONS/" ${PATH_PAGES}/page4.svg > tmp/"$DESTINATAIRE"/page4.svg
 
-sed '/%GRILLE%/Q' ${PATH_PAGES}page3.svg > tmp/"$DESTINATAIRE"/page3.svg
+sed '/%GRILLE%/Q' ${PATH_PAGES}/page3.svg > tmp/"$DESTINATAIRE"/page3.svg
 sed '1d' $GRILLE >> tmp/"$DESTINATAIRE"/page3.svg
-sed '1,/%GRILLE%/d' ${PATH_PAGES}page3.svg >> tmp/"$DESTINATAIRE"/page3.svg
+sed '1,/%GRILLE%/d' ${PATH_PAGES}/page3.svg >> tmp/"$DESTINATAIRE"/page3.svg
 
-sed "s/%SOLUTIONS%/$SOLUTIONS/" ${PATH_PAGES}page4.svg > /tmp/"$DESTINATAIRE"/page4.svg
+sed "s/%SOLUTIONS%/$SOLUTIONS/" ${PATH_PAGES}/page4.svg > /tmp/"$DESTINATAIRE"/page4.svg
 
 
 
