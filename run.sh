58075e48fe348e6e6fdf938d2d01786d467a8ba2
Tangui Morlier <tangui@tangui.eu.org>
2026-01-28 11:31:53 +0100

    Discret

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ if ! test "$1"; then
 fi
 
 rm -f txt
-wget "$1"
+wget -q "$1"
 grep -v '#' txt  | grep ';' | grep -v gabriel,charlene,vincent | awk -F ';' '{if ($5) { dir=toupper(gensub(" ", "_", "g", $5"_"$1)); print "test -d output/"dir" || bash replace.sh \""$2"\" \""$1"\" \"output/"dir"\" \"pages/"$3"\" dictionnaires/"$4".json"} }'
